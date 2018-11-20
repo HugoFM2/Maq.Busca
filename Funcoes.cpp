ac8619b8fcbe4cb22c980526a1e7a3f5a53a1939
Hugo Ferreira <hugofm2@ufmg.br>
2018-11-20 13:33:03 -0200

    Troca do std

--- Funcoes.cpp
+++ Funcoes.cpp
@@ -1,30 +1,30 @@
 #include "Funcoes.h"
 
-void AbrirArquivo(std::set <std::string> &words,std::string filename){
+void AbrirArquivo(set <string> &words,string filename){
 	try{ // parte protegia do codigo, caso o arquivo nao esteja aberto entra no catch
-	std::fstream file (filename);
-	std::string Palavra;
+	fstream file (filename);
+	string Palavra;
 	if (file.is_open()){                      //testa se o arquivo foi aberto com sucesso
 		while(file >> Palavra){	   //Adiciona todas as palavras do texto a string "Palavra"
 			words.insert(Palavra);
 		}
 		file.close();
 	}else {throw 1;}
-}catch(int erro){std::cout<<"*ATENCAO*: O ultimo arquivo digitado nao foi encontrado, verifique se o mesmo existe e tente novamente.\n";} // mensagem exibida caso nao consiga abrir o arquivo
+}catch(int erro){cout<<"*ATENCAO*: O ultimo arquivo digitado nao foi encontrado, verifique se o mesmo existe e tente novamente.\n";} // mensagem exibida caso nao consiga abrir o arquivo
 }
 
-void SubstituirString(std::string &Palavra) {
-	std::regex pontuacao("(\\.|,|!|-|\\?)"); // detecta as pontuacoes
-	Palavra = std::regex_replace(Palavra,pontuacao,""); // Remove pontuacao (ponto, virgula, hifen e exclamacao)
+void SubstituirString(string &Palavra) {
+	regex pontuacao("(\\.|,|!|-|\\?)"); // detecta as pontuacoes
+	Palavra = regex_replace(Palavra,pontuacao,""); // Remove pontuacao (ponto, virgula, hifen e exclamacao)
 	for(int j = 0; j<Palavra.length();j++){ // Transformar todas as letras maiusculas em minusculas
 		Palavra[j] = tolower(Palavra[j]);
 	}
 }
 
-void SubstituirSet(std::set <std::string> &words) {
-	std::set <std::string> temp; // Set temporario para guardar as palavras novas enquanto recria o set com as palavras modificadas
-	for (std::set<std::string>::iterator i=words.begin(); i!=words.end(); ++i) {
-		std::string Palavra;
+void SubstituirSet(set <string> &words) {
+	set <string> temp; // Set temporario para guardar as palavras novas enquanto recria o set com as palavras modificadas
+	for (set<string>::iterator i=words.begin(); i!=words.end(); ++i) {
+		string Palavra;
 		Palavra = *i;
 		SubstituirString(Palavra);
 
@@ -36,12 +36,12 @@ void SubstituirSet(std::set <std::string> &words) {
 }
 
 
-void SetToMap(std::set <std::string> words, std::map <std::string,std::string> &summary,std::string chave) {
-	for (std::set<std::string>::iterator i=words.begin(); i!=words.end(); ++i) {  //faz com que o iterator comece no inicio do set e avance uma unidade por vez, ate o final
+void SetToMap(set <string> words, map <string,string> &summary,string chave) {
+	for (set<string>::iterator i=words.begin(); i!=words.end(); ++i) {  //faz com que o iterator comece no inicio do set e avance uma unidade por vez, ate o final
 			summary[*i] += chave + "\n";
 	}
 }
 
-void PrintMap( std::map <std::string,std::string> map,std::string chave) {
-	std::cout<<map[chave]<<std::endl; //printa na tela os documentos em relaçao a determinada chave
+void PrintMap( map <string,string> map,string chave) {
+	cout<<map[chave]<<endl; //printa na tela os documentos em relaçao a determinada chave
 }
